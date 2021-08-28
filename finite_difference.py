daf643718ea12f6ffd7d161edb37925bc25463d7
Zachariah B. Etienne <zachetie@gmail.com>
2021-08-28 09:20:06 -0700

    finite_difference.py: Fix comment.

--- finite_difference.py
+++ finite_difference.py
@@ -191,11 +191,11 @@ def register_C_functions_and_NRPy_basic_defines(NGHOSTS_account_for_onezone_upwi
     NGHOSTS = int(par.parval_from_str("finite_difference::FD_CENTDERIVS_ORDER")/2)
     if NGHOSTS_account_for_onezone_upwind:
         NGHOSTS += 1
-    Nbd_str  = """
+        Nbd_str = """
 // Set the number of ghost zones
-// Note that upwinding in BSSN requires that NGHOSTS = FD_CENTDERIVS_ORDER/2 + 1 <- Notice the +1.
+// Note that upwinding in e.g., BSSN requires that NGHOSTS = FD_CENTDERIVS_ORDER/2 + 1 <- Notice the +1.
 """
-    Nbd_str  = "#define NGHOSTS " + str(NGHOSTS)+"\n"
+    Nbd_str += "#define NGHOSTS " + str(NGHOSTS)+"\n"
     outC_NRPy_basic_defines_h_dict["finite_difference"] = Nbd_str
 
 
