00b1e30083247741e9124a717f4c0fe4990d0343
Zachariah B. Etienne <zachetie@gmail.com>
2021-08-29 10:19:25 -0700

    UnitTesting/all_notebooks_lint_in_pyconv.sh: Add for quick linting of all core notebooks

---
+++ UnitTesting/all_notebooks_lint_in_pyconv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir pyconv
+
+rm -f /tmp/joblist.txt /tmp/joblist2.txt
+for i in *.ipynb; do
+    # FIXME: Don't know why Tutorial-TOV-Piecewise_Polytrope_EOSs.ipynb.py causes pylint to choke...
+    if [ $i != "Tutorial-Start_to_Finish-BaikalETK.ipynb" ] && [ $i != "NRPyPlus_Tutorial.ipynb" ] && [ $i != "Tutorial-How_NRPy_Computes_Finite_Difference_Coeffs.ipynb" ] && [ $i != "Tutorial-TOV-Piecewise_Polytrope_EOSs.ipynb" ] ; then
+       echo "jupyter nbconvert --to python $i --stdout |grep -v \"^\# \" > pyconv/$i.py" >> /tmp/joblist.txt  # ignore lines that start with #.
+       echo "pylint --disable=trailing-newlines,reimported,ungrouped-imports --output=pyconv/$i.py.txt pyconv/$i.py" >> /tmp/joblist2.txt
+    fi
+done
+
+parallel --jobs 16 < /tmp/joblist.txt
+parallel --jobs 16 < /tmp/joblist2.txt
+
+echo "TOP OFFENDERS:"
+for i in pyconv/*.txt; do echo `grep rated $i` $i;done |sort -k7 -g|head -n20
