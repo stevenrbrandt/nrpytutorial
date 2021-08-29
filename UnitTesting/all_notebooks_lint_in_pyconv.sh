18e5f5de1b41082f0c5f47dac61b9da26a080fa2
Zachariah B. Etienne <zachetie@gmail.com>
2021-08-29 10:31:15 -0700

    UnitTesting/all_notebooks_lint_in_pyconv.sh: Fix a couple of bugs

--- UnitTesting/all_notebooks_lint_in_pyconv.sh
+++ UnitTesting/all_notebooks_lint_in_pyconv.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+rm -rf pyconv
 mkdir pyconv
 
 rm -f /tmp/joblist.txt /tmp/joblist2.txt
 for i in *.ipynb; do
     # FIXME: Don't know why Tutorial-TOV-Piecewise_Polytrope_EOSs.ipynb.py causes pylint to choke...
-    if [ $i != "Tutorial-Start_to_Finish-BaikalETK.ipynb" ] && [ $i != "NRPyPlus_Tutorial.ipynb" ] && [ $i != "Tutorial-How_NRPy_Computes_Finite_Difference_Coeffs.ipynb" ] && [ $i != "Tutorial-TOV-Piecewise_Polytrope_EOSs.ipynb" ] ; then
+    if [ $i != "Tutorial-BaikalETK.ipynb" ] && [ $i != "NRPyPlus_Tutorial.ipynb" ] && [ $i != "Tutorial-How_NRPy_Computes_Finite_Difference_Coeffs.ipynb" ] && [ $i != "Tutorial-TOV-Piecewise_Polytrope_EOSs.ipynb" ] ; then
        echo "jupyter nbconvert --to python $i --stdout |grep -v \"^\# \" > pyconv/$i.py" >> /tmp/joblist.txt  # ignore lines that start with #.
-       echo "pylint --disable=trailing-newlines,reimported,ungrouped-imports --output=pyconv/$i.py.txt pyconv/$i.py" >> /tmp/joblist2.txt
+       echo "echo \"Just finished linting $i.\" ; pylint --disable=trailing-newlines,reimported,ungrouped-imports --output=pyconv/$i.py.txt pyconv/$i.py" >> /tmp/joblist2.txt
     fi
 done
 
