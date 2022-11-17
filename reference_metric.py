0ba9186c2b3dfb8d5ae73965f97a648df62578f9
Zach Etienne <zachetie BaikalETK benchmark-z4cnrpy.par BHAH BSSN cactusthorn.py carpet.sh carpetx.sh cmdline_helper.py colored.py _config.yml convert_jupyter_to_python_and_run.sh cse_helpers.py CurviBoundaryConditions expr.md expr_tree.py finite_difference_helpers.py finite_difference.py FishboneMoncriefID functional.py GRFFE GRHD grid.py GRMHD here.py IllinoisGRMHD indexedexp.py __init__.py in_progress jupyter_notebook_config.py latex_nrpy_style.tplx LICENSE lint_Jupyter_notebook.sh loop.py make.out Maxwell MaxwellVacuum Min_Max_and_Piecewise_Expressions.py MoLtimestepping NRPyEOS NRPy_logo.py nrpy.out NRPy_param_funcs.py NRPyPlus_Tutorial.ipynb NRPyPN nrpy.py nrpy-run.par outputC.py param.txt pickling.py __pycache__ README.md reference_metric.py requirements.txt run_Jupyter_notebook.sh run.out safewrite.py sbPoynETNRPy ScalarField ScalarWave SEOBNR SIMD SIMD.py SommerfeldBoundaryCondition SpinWeight_minus2_SphHarmonics src suffixes.py sugar.py tensor_rotation.py test1.py test2.py test3.py tests TOV t.py Tutorial-ADMBSSN_tofrom_4metric.ipynb Tutorial-ADM_Initial_Data-Brill-Lindquist.ipynb Tutorial-ADM_Initial_Data-Converting_Exact_ADM_Spherical_or_Cartesian_to_BSSNCurvilinear.ipynb Tutorial-ADM_Initial_Data-Converting_Numerical_ADM_Spherical_or_Cartesian_to_BSSNCurvilinear.ipynb Tutorial-ADM_Initial_Data-ScalarField.ipynb Tutorial-ADM_Initial_Data-ShiftedKerrSchild.ipynb Tutorial-ADM_Initial_Data-StaticTrumpet.ipynb Tutorial-ADM_Initial_Data-TOV.ipynb Tutorial-ADM_Initial_Data-UIUC_BlackHole.ipynb Tutorial-ADM_in_terms_of_BSSN.ipynb Tutorial-BSSN-basis_transforms.ipynb Tutorial-BSSN_constraints.ipynb Tutorial-BSSN_enforcing_determinant_gammabar_equals_gammahat_constraint.ipynb Tutorial-BSSN_formulation.ipynb Tutorial-BSSN_in_terms_of_ADM.ipynb Tutorial-BSSN_quantities.ipynb Tutorial-BSSN_stress_energy_source_terms.ipynb Tutorial-BSSN_time_evolution-BSSN_gauge_RHSs.ipynb Tutorial-BSSN_time_evolution-BSSN_RHSs.ipynb Tutorial-BSSN_time_evolution-C_codegen_library.ipynb Tutorial-cmdline_helper.ipynb Tutorial-Coutput__Parameter_Interface.ipynb Tutorial-Derivation_of_GRMHD_Evolution_Equations.ipynb Tutorial-ETK_thorn-BaikalETK.ipynb Tutorial-ETK_thorn-FishboneMoncriefID.ipynb Tutorial-ETK_thorn-IDScalarWaveNRPy.ipynb Tutorial-ETK_thorn-Interpolation_to_Arbitrary_Grids_multi_order.ipynb Tutorial-ETK_thorn-Interpolation_to_Spherical_Grids.ipynb Tutorial-ETK_thorn-Interpolation_to_Spherical_Grids_multi_order.ipynb Tutorial-ETK_thorn-MaxwellVacuumID.ipynb Tutorial-ETK_thorn-MaxwellVacuum.ipynb Tutorial-ETK_thorn-NRPyPlusTOVID.ipynb Tutorial-ETK_thorn-u0_smallb_Poynting.ipynb Tutorial-ETK_thorn-WaveToyNRPy.ipynb Tutorial-ETK_thorn-Weyl_Scalars_and_Spacetime_Invariants.ipynb Tutorial-Finite_Difference_Derivatives-FDtable_soln.ipynb Tutorial-Finite_Difference_Derivatives.ipynb Tutorial-FishboneMoncriefID.ipynb Tutorial-GRFFE_Equations-Cartesian.ipynb Tutorial-GRHD_Equations-Cartesian-c-code.ipynb Tutorial-GRHD_Equations-Cartesian.ipynb Tutorial-GRHD_UnitConversion.ipynb Tutorial-GRMHD_Equations-Cartesian.ipynb Tutorial-How_NRPy_Computes_Finite_Difference_Coeffs.ipynb Tutorial-Indexed_Expressions.ipynb Tutorial-Indexed_Expressions_soln.ipynb Tutorial-LaTeX_Interface_Example-BSSN_Cartesian.ipynb Tutorial-Loop_Generation_Cache_Blocking.ipynb Tutorial-Loop_Generation_Cache_Blocking_soln.ipynb Tutorial-MaxwellCartesian.ipynb Tutorial-MaxwellCurvilinear.ipynb Tutorial-Method_of_Lines-C_Code_Generation.ipynb Tutorial-Method_of_Lines-C_Code_Generation_new_way.ipynb Tutorial-Min_Max_and_Piecewise_Expressions.ipynb Tutorial-NRPyPlus_10_Minute_Overview.ipynb Tutorial-NS_radial_oscillations.ipynb Tutorial-Numerical_Grids.ipynb Tutorial-Psi4.ipynb Tutorial-Psi4_tetrads.ipynb Tutorial-Reference_Metric.ipynb Tutorial-RK_Butcher_Table_Dictionary.ipynb Tutorial-RK_Butcher_Table_Validation.ipynb Tutorial-ScalarField_RHSs.ipynb Tutorial-ScalarField_Tmunu.ipynb Tutorial-ScalarWaveCurvilinear.ipynb Tutorial-ScalarWave.ipynb Tutorial-SEOBNR_Derivative_Routine.ipynb Tutorial-SEOBNR_v3_Hamiltonian.ipynb Tutorial-SEOBNR_v4P_Hamiltonian.ipynb Tutorial-Solving_the_Scalar_Wave_Equation_with_NumPy_files Tutorial-Solving_the_Scalar_Wave_Equation_with_NumPy.ipynb Tutorial-SommerfeldBoundaryCondition.ipynb Tutorial-SpinWeighted_Spherical_Harmonics.ipynb Tutorial-Start_to_Finish-BSSNCurvilinear-Neutron_Star-Hydro_without_Hydro.ipynb Tutorial-Start_to_Finish-BSSNCurvilinear-ScalarField_Collapse.ipynb Tutorial-Start_to_Finish-BSSNCurvilinear-Setting_up_Exact_Initial_Data.ipynb Tutorial-Start_to_Finish-BSSNCurvilinear-Setting_up_ScalarField_initial_data.ipynb Tutorial-Start_to_Finish-BSSNCurvilinear-Setting_up_TOV_initial_data.ipynb Tutorial-Start_to_Finish-BSSNCurvilinear-Two_BHs_Collide.ipynb Tutorial-Start_to_Finish-BSSNCurvilinear-Two_BHs_Collide_new_way.ipynb Tutorial-Start_to_Finish-BSSNCurvilinear-Two_BHs_Collide-no_rfm_precompute.ipynb Tutorial-Start_to_Finish-BSSNCurvilinear-Two_BHs_Collide-Psi4.ipynb Tutorial-Start_to_Finish-BSSNCurvilinear-Two_BHs_Collide-Psi4_new_way.ipynb Tutorial-Start_to_Finish-Curvilinear_BCs.ipynb Tutorial-Start_to_Finish-Curvilinear_BCs_new_way.ipynb Tutorial-Start_to_Finish-Finite_Difference_Playground.ipynb Tutorial-Start_to_Finish-Finite_Difference_Playground_new_way.ipynb Tutorial-Start_to_Finish-FishboneMoncriefID_standalone.ipynb Tutorial-Start_to_Finish-ScalarWaveCurvilinear.ipynb Tutorial-Start_to_Finish-ScalarWaveCurvilinear_new_way.ipynb Tutorial-Start_to_Finish-ScalarWave.ipynb Tutorial-Start_to_Finish-ScalarWave_new_way.ipynb Tutorial-Start_to_Finish-Solving_Maxwells_Equations_in_Vacuum-Cartesian.ipynb Tutorial-Start_to_Finish-Solving_Maxwells_Equations_in_Vacuum-Curvilinear.ipynb Tutorial-Symbolic_Tensor_Rotation.ipynb Tutorial-SymPy_LaTeX_Interface.ipynb Tutorial-Template_Style_Guide.ipynb Tutorial-TOV-Piecewise_Polytrope_EOSs.ipynb Tutorial-u0_smallb_Poynting-Cartesian.ipynb Tutorial-UnitTesting.ipynb Tutorial-VacuumMaxwell_Cartesian_RHSs.ipynb Tutorial-VacuumMaxwell_Curvilinear_RHSs.ipynb Tutorial-VacuumMaxwell_formulation_Cartesian.ipynb Tutorial-VacuumMaxwell_formulation_Curvilinear.ipynb Tutorial-VacuumMaxwell_InitialData.ipynb Tutorial-WeylScalarsInvariants-Cartesian.ipynb u0_smallb_Poynting__Cartesian UnitTesting wave2d.par WaveToyNRPy WaveToy.py WeylScal4NRPy x x1 x2 x2.py x.py x.sh x.txt xx y z4c.out Z4c.py z4c.sh Z4c-sugar.py z4.out at* gmail>
2022-11-17 09:59:17 -0800

    reference_metric.py: Remove deprecated and unused function add_to_Cfunction_dict_find_timestep().

--- reference_metric.py
+++ reference_metric.py
@@ -1455,38 +1455,6 @@ def add_to_Cfunc_dict_xx_to_Cart(rel_path_to_Cparams=os.path.join("./")):
         rel_path_to_Cparams=rel_path_to_Cparams)
 
 
-# Find the appropriate timestep for the CFL condition.
-def add_to_Cfunction_dict_find_timestep():
-    # Compute proper distance in all 3 directions.
-    delxx = ixp.declarerank1("dxx", DIM=3)
-    ds_drn = ds_dirn(delxx)
-
-    ds_dirn_h = outputC([ds_drn[0], ds_drn[1], ds_drn[2]], ["ds_dirn0", "ds_dirn1", "ds_dirn2"],"returnstring")
-
-    desc="Find the CFL-constrained timestep"
-    add_to_Cfunction_dict(
-        desc     =desc,
-        c_type   ="REAL",
-        name     ="find_timestep",
-        params   ="const paramstruct *restrict params, REAL *restrict xx[3]",
-        preloop  ="REAL dsmin = 1e38; // Start with a crazy high value... close to the largest number in single precision.",
-        body     ="REAL ds_dirn0, ds_dirn1, ds_dirn2;\n"+ds_dirn_h+"""
-#ifndef MIN
-#define MIN(A, B) ( ((A) < (B)) ? (A) : (B) )
-#endif
-        // Set dsmin = MIN(dsmin, ds_dirn0, ds_dirn1, ds_dirn2);
-        dsmin = MIN(dsmin,MIN(ds_dirn0,MIN(ds_dirn1,ds_dirn2)));
-""",
-        loopopts ="InteriorPoints,Read_xxs,DisableOpenMP",
-        postloop ="return dsmin*CFL_FACTOR/wavespeed;\n")
-
-
-def out_timestep_func_to_file(outfile):
-    add_to_Cfunction_dict_find_timestep()
-    with open(outfile, "w") as file:
-        file.write(outC_function_dict["find_timestep"])
-
-
 def register_NRPy_basic_defines(enable_rfm_precompute=False):
     Nbd = ""
     if enable_rfm_precompute:
