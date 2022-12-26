00f138848ecd58162f0c3a47405c990f95ac915c
Zachariah B. Etienne <zachetie@gmail.com>
2022-12-26 10:30:40 -0800

    Move all Maxwell notebooks to in_progress-Maxwell. Remove from main NRPy+ menu for now. Maxwell notebooks are in serious need of modernization and revision.

--- UnitTesting/run_NRPy_UnitTests.sh
+++ UnitTesting/run_NRPy_UnitTests.sh
@@ -47,7 +47,7 @@ add_test GRMHD/tests/test_GRMHD.py
 add_test FishboneMoncriefID/tests/test_FishboneMoncriefID.py
 add_test in_progress/GiRaFFE_HO/tests/test_GiRaFFE_HO.py
 add_test in_progress/GiRaFFEfood_HO/tests/test_GiRaFFEfood_HO.py
-add_test Maxwell/tests/test_Maxwell.py
+#add_test Maxwell/tests/test_Maxwell.py
 add_test ScalarWave/tests/test_ScalarWave.py
 add_test tests/test_reference_metric.py
 add_test u0_smallb_Poynting__Cartesian/tests/test_u0_smallb_Poynting__Cartesian.py
