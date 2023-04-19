ea991fe9fc9c2cef706588d6dfdd95487eaa673c
Zach Etienne <zachetie ** at* gmail>
2023-04-19 12:23:11 -0700

    ScalarWave/tests/test_ScalarWave.py: update ID options

--- ScalarWave/tests/trusted_values_dict.py
+++ ScalarWave/tests/trusted_values_dict.py
@@ -11,8 +11,8 @@ trusted_values_dict['ScalarWave_RHSs__ScalarWave_RHSs__globals'] = {'wavespeed':
 trusted_values_dict['ScalarWaveCurvilinear_RHSs__ScalarWaveCurvilinear_RHSs__globals'] = {'uu_rhs': mpf('0.65261108714780824424650518267299'), 'vv_rhs': mpf('7.91102076566947763349564936041703')}
 
 # Generated on: 2020-05-23
-trusted_values_dict['InitialData__InitialData__Type__PlaneWave___globals'] = {'uu_ID': mpf('2.52358067004083477110217386122'), 'vv_ID': mpf('-0.569645247209539485189673979502')}
+trusted_values_dict['InitialData__InitialData__WaveType__PlaneWave___globals'] = {'uu_ID': mpf('2.52358067004083477110217386122'), 'vv_ID': mpf('-0.569645247209539485189673979502')}
 
 # Generated on: 2022-11-11
 # Notes: Added 2 to uu, so that relative error is well-defined everywhere.
-trusted_values_dict['InitialData__InitialData__Type__SphericalGaussian___globals'] = {'uu_ID': mpf('2.36513405713036977689022106748'), 'vv_ID': mpf('0.0563512459589975646763098472872')}
+trusted_values_dict['InitialData__InitialData__WaveType__SphericalGaussian___globals'] = {'uu_ID': mpf('2.36513405713036977689022106748'), 'vv_ID': mpf('0.0563512459589975646763098472872')}
