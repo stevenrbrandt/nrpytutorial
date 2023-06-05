456e135c4d243a380e856935909a45400249042f
Zachariah B. Etienne <zachetie@gmail.com>
2023-06-04 18:28:14 -0700

    UnitTesting/RepeatedTimer.py: Add comments

--- UnitTesting/RepeatedTimer.py
+++ UnitTesting/RepeatedTimer.py
@@ -2,9 +2,20 @@
 
 from threading import Timer
 
-
 class RepeatedTimer(object):
+    """
+    A Timer class that can executes a function every 'interval' seconds.
+    """
+
     def __init__(self, interval, function, *args, **kwargs):
+        """
+        Creates a timer.
+
+        :param interval: int or float, the number of seconds between each call to 'function'
+        :param function: function, the function to call
+        :param args: list, optional, positional arguments to pass to 'function'
+        :param kwargs: dict, optional, keyword arguments to pass to 'function'
+        """
         self._timer     = None
         self.interval   = interval
         self.function   = function
@@ -14,16 +25,29 @@ class RepeatedTimer(object):
         self.start()
 
     def _run(self):
+        """
+        Is called every 'interval' seconds and runs 'function'.
+        """
         self.is_running = False
         self.start()
-        self.function(*self.args, **self.kwargs)
+        try:
+            self.function(*self.args, **self.kwargs)
+        except Exception as e:
+            # You could add better exception handling here
+            print(str(e))
 
     def start(self):
+        """
+        Starts the timer.
+        """
         if not self.is_running:
             self._timer = Timer(self.interval, self._run)
             self._timer.start()
             self.is_running = True
 
     def stop(self):
+        """
+        Stops the timer.
+        """
         self._timer.cancel()
         self.is_running = False
