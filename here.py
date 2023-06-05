bec94c6cc94b2421b59ec1b83fddc5981e33fe99
Zachariah B. Etienne <zachetie@gmail.com>
2023-06-04 17:16:53 -0700

    here.py: reformat for PIP-8

--- here.py
+++ here.py
@@ -6,18 +6,21 @@ import sys
 
 _here = os.path.realpath(os.getcwd())
 
+
 def here(*args):
-    herell(False,*args)
+    herell(False, *args)
+
 
 def herecc(*args):
-    herell(True,*args)
+    herell(True, *args)
 
-def herell(usecc,*args):
+
+def herell(usecc, *args):
     import inspect
     stack = inspect.stack()
     frame = stack[2]
     if usecc:
-        herestr = re.sub(r"^herecc\((.*)\)$",r"HERE: \1:",frame.code_context[0].strip())
+        herestr = re.sub(r"^herecc\((.*)\)$", r"HERE: \1:", frame.code_context[0].strip())
     else:
         herestr = "HERE:"
     if isinstance(frame, tuple):
@@ -28,15 +31,16 @@ def herell(usecc,*args):
         fname = os.path.realpath(frame.filename)
         line = frame.lineno
     if fname.startswith(_here):
-        fname = fname[len(_here)+1:]
+        fname = fname[len(_here) + 1:]
     assert isinstance(fname, str)
     assert isinstance(line, int)
-    nargs = [colored(herestr,"cyan"),fname+":"+colored(line,"yellow")] + list(args) #, flush=True)
+    nargs = [colored(herestr, "cyan"), fname + ":" + colored(line, "yellow")] + list(args)  # , flush=True)
     print(*nargs)
     sys.stdout.flush()
     sys.stderr.flush()
     frame = None
     stack = None
+
 
 if __name__ == "__main__":
     here(_here)
